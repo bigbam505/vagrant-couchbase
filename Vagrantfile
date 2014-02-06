921ca251a3971a2da95de4366cdcb4dfc4382933
Montague, Brent <bmontague@cvent.com>
2014-02-06 02:34:57 -0500

    Add masternode and two node deployment

--- Vagrantfile
+++ Vagrantfile
@@ -12,15 +12,25 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   ipAddrPrefix = "192.168.56.10"
 
   config.vm.provider "virtualbox" do |v|
-    v.customize ["modifyvm", :id, "--memory", 1024]
+    v.customize ["modifyvm", :id, "--memory", 756]
   end
 
   config.vm.provision :puppet
+
+  config.vm.define "masternode" do |node|
+    node.vm.box = "precise64"
+    node.vm.hostname = "masternode"
+    node.vm.network :private_network, ip: "192.168.56.110"
+    node.vm.provider "virtualbox" do |v|
+      v.name = "Couchbase Server master Node"
+    end
+  end
 
   1.upto(numNodes) do |num|
     nodeName = ("node" + num.to_s).to_sym
     config.vm.define nodeName do |node|
       node.vm.box = "precise64"
+      node.vm.hostname = nodeName
       node.vm.network :private_network, ip: ipAddrPrefix + num.to_s
       node.vm.provider "virtualbox" do |v|
         v.name = "Couchbase Server Node " + num.to_s
--- manifests/default.pp
+++ manifests/default.pp
@@ -1,17 +1,49 @@
-exec { 'couchbase-server-source':
-  command => '/usr/bin/wget http://packages.couchbase.com/releases/2.0.1/couchbase-server-enterprise_x86_64_2.0.1.deb',
-      cwd => '/home/vagrant/',
-  creates => '/home/vagrant/couchbase-server-enterprise_x86_64_2.0.1.deb',
-   before => Package['couchbase-server']
-}
+class couchbase {
+  exec { 'couchbase-server-source':
+    command => '/usr/bin/wget http://packages.couchbase.com/releases/2.0.1/couchbase-server-enterprise_x86_64_2.0.1.deb',
+    cwd => '/home/vagrant/',
+    creates => '/home/vagrant/couchbase-server-enterprise_x86_64_2.0.1.deb',
+    before => Package['couchbase-server']
+  }
 
-exec { 'install-deps':
-  command => '/usr/bin/apt-get install libssl0.9.8',
-   before => Package['couchbase-server']
-}
+  exec { 'install-deps':
+    command => '/usr/bin/apt-get install libssl0.9.8',
+    before => Package['couchbase-server']
+  }
 
-package { 'couchbase-server':
-  provider => dpkg,
+  package { 'couchbase-server':
+    provider => dpkg,
     ensure => installed,
     source => '/home/vagrant/couchbase-server-enterprise_x86_64_2.0.1.deb'
+  }
 }
+
+node 'masternode' {
+  include couchbase
+  exec { 'init-couchbase-cluster':
+    command => '/opt/couchbase/bin/couchbase-cli cluster-init -c 127.0.0.1:8091 --cluster-init-username=admin --cluster-init-password=password --cluster-init-ramsize=512',
+    require => Package['couchbase-server']
+  }
+
+  exec { 'init-couchbase-bucket':
+    command => '/opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=default --bucket-type=couchbase --bucket-ramsize=256 --bucket-replica=0 -u admin -p password',
+    require => Exec['init-couchbase-cluster']
+  }
+}
+
+node 'node1' {
+  include couchbase
+  exec { 'add-node-cluster':
+    command => '/opt/couchbase/bin/couchbase-cli rebalance -c 192.168.56.110 --server-add=192.168.56.101 -u admin -p password',
+    require => Package['couchbase-server']
+  }
+}
+
+node 'node2' {
+  include couchbase
+  exec { 'add-node-to-cluster':
+    command => '/opt/couchbase/bin/couchbase-cli rebalance -c 192.168.56.110 --server-add=192.168.56.102 -u admin -p password',
+    require => Package['couchbase-server']
+  }
+}
+
